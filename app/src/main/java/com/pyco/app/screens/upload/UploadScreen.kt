27050c4cc001c3ba2e5b709e3aa0c4cd16bde16c
Akemi Sai <126123381+achemai@users.noreply.github.com>
2024-12-02 17:07:10 -0500

    camerax api

--- app/src/main/java/com/pyco/app/screens/upload/UploadScreen.kt
+++ app/src/main/java/com/pyco/app/screens/upload/UploadScreen.kt
@@ -6,20 +6,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.pyco.app.MainActivity
+import com.pyco.app.base.CameraPreview
 import com.pyco.app.components.BottomNavigationBar
 
 @Composable
 fun UploadScreen(
     navController: NavHostController
 ) {
+    val context = LocalContext.current as MainActivity
+    val permissionGranted = context.isCameraPermissionGranted.collectAsState().value
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -27,7 +34,13 @@ fun UploadScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to the Upload Screen!")
+        if (permissionGranted) {
+            CameraPreview()
+        } else {
+            Button(onClick = { context.handleCameraPermission() }) {
+                Text("Request Camera Permission")
+            }
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -42,7 +55,6 @@ fun UploadScreen(
     ) {
         BottomNavigationBar(navController = navController)
     }
-
 }
 
 @Preview(showBackground = true, device = "id:pixel_6_pro", name = "fone")
