62ac568cfe00a449573e1d69b6d33af03e68f5ce
Ed M <emarecos@bu.edu>
2024-12-02 17:32:00 -0500

    nom

--- app/src/main/java/com/pyco/app/screens/upload/UploadScreen.kt
+++ app/src/main/java/com/pyco/app/screens/upload/UploadScreen.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.pyco.app.MainActivity
+import com.pyco.app.base.CameraPreview
 import com.pyco.app.components.BottomNavigationBar
 import com.pyco.app.components.backgroundColor
 import com.pyco.app.components.customColor
@@ -24,17 +28,20 @@ import com.pyco.app.components.customColor
 fun UploadScreen(
     navController: NavHostController
 ) {
+    val context = LocalContext.current as MainActivity
+    val permissionGranted = context.isCameraPermissionGranted.collectAsState().value
+
     Scaffold(
         bottomBar = {
             BottomNavigationBar(navController = navController)
         },
-        containerColor = backgroundColor // Sets the overall background color
+        containerColor = backgroundColor
     ) { paddingValues ->
         Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            color = backgroundColor // Explicitly set the background color here
+            color = backgroundColor
         ) {
             Column(
                 modifier = Modifier
@@ -43,15 +50,16 @@ fun UploadScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Welcome Text
-                Text(
-                    text = "Welcome to the Upload Screen!",
-                    color = customColor
-                )
+                if (permissionGranted) {
+                    CameraPreview()
+                } else {
+                    Button(onClick = { context.handleCameraPermission() }) {
+                        Text("Request Camera Permission", color = customColor)
+                    }
+                }
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Informative Text
                 Text(
                     text = "Use the navigation bar to go to other sections.",
                     color = customColor
