7d715336020f80cadecb5aa561f23bd1cd7bc905
Ed M <emarecos@bu.edu>
2024-12-16 01:50:44 -0500

    oops forgot to delete this

--- app/src/main/java/com/pyco/app/models/User.kt
+++ app/src/main/java/com/pyco/app/models/User.kt
@@ -17,9 +17,3 @@ data class User(
     val followingCount: Int = 0,                            // Count of following
     val likesCount: Int = 0                                 // Total number of likes given + received
 )
-
-data class Like(
-    val userId: String = "",                                // User who liked the outfit
-    val outfitId: String = "",                              // Outfit that was liked
-    val timestamp: Timestamp = Timestamp.now()              // Time when the like was given
-)
