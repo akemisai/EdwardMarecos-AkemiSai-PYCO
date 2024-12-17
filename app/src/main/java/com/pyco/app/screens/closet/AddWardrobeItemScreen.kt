616e2d3b66b59a21e724763c48d6317b799d9a2b
Akemi Sai <126123381+achemai@users.noreply.github.com>
2024-12-17 15:19:17 -0500

    add item UI

--- app/src/main/java/com/pyco/app/screens/closet/AddWardrobeItemScreen.kt
+++ app/src/main/java/com/pyco/app/screens/closet/AddWardrobeItemScreen.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,20 +16,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -36,14 +46,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
+import com.pyco.app.MainActivity
+import com.pyco.app.R
+import com.pyco.app.components.backgroundColor
+import com.pyco.app.components.customColor
 import com.pyco.app.models.ClothingItem
 import com.pyco.app.models.ClothingType
 import com.pyco.app.models.Colors
@@ -61,6 +83,7 @@ fun AddWardrobeItemScreen(
 ) {
 
     // State variables for form fields
+    val context = LocalContext.current as MainActivity
     var name by remember { mutableStateOf("") }
     var type by remember { mutableStateOf(ClothingType.TOP) }
     var colors by remember { mutableStateOf(Colors.BLACK) }
@@ -107,22 +130,39 @@ fun AddWardrobeItemScreen(
                     IconButton(onClick = { navController.navigateUp() }) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = backgroundColor,
+                    titleContentColor = customColor,
+                    navigationIconContentColor = customColor
+                )
             )
-        }
+        },
+        containerColor = backgroundColor
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
                 .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(7.dp)
         ) {
             OutlinedTextField(
                 value = name,
                 onValueChange = { name = it },
                 label = { Text("Name") },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = customColor,
+                    unfocusedBorderColor = customColor,
+                    cursorColor = customColor,
+                    focusedPlaceholderColor = customColor,
+                    unfocusedPlaceholderColor = customColor,
+                    focusedTextColor = customColor,
+                    unfocusedTextColor = customColor,
+                    focusedLabelColor = customColor,
+                    unfocusedLabelColor = customColor
+                )
             )
 
             DropdownMenuBox(
@@ -145,22 +185,32 @@ fun AddWardrobeItemScreen(
                 selectedOption = material,
                 onOptionSelected = { material = it }
             )
+            Text(
+                text = "Select Image",
+                color = customColor,
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier
+                    .clickable { launcher.launch("image/*") }
 
-            Button(onClick = { launcher.launch("image/*") }) {
-                Text("Select Image")
-            }
+            )
 
             imageUrl?.let {
                 Image(
                     painter = rememberAsyncImagePainter(it),
                     contentDescription = "Selected Image",
-                    modifier = Modifier.size(128.dp)
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .clip(MaterialTheme.shapes.medium)
+                        .fillMaxWidth()
+                        .height(350.dp)
+                        .background(Color.Black)
                 )
             }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            ExtendedFloatingActionButton(
                 onClick = {
                     Log.d("AddWardrobeItem", "Save Button Clicked")
                     if (name.isNotBlank() && imageUrl != null) {
@@ -194,10 +244,22 @@ fun AddWardrobeItemScreen(
                         Log.e("AddWardrobeItem", "Validation failed: Ensure all fields are filled.")
                     }
                 },
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
-            ) {
-                Text("Save Item", color = Color.White)
-            }
+                containerColor = customColor,
+                contentColor = backgroundColor,
+                content = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_save),
+                        contentDescription = "Save Item",
+                        tint = backgroundColor,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("Save Item", color = backgroundColor)
+                },
+                modifier = Modifier
+                    .align(BottomCenter)
+                    .padding(40.dp)
+            )
         }
     }
 }
@@ -214,15 +276,23 @@ fun <T> DropdownMenuBox(
     var expanded by remember { mutableStateOf(false) }
 
     Column {
-        Text(text = label, style = MaterialTheme.typography.labelSmall)
+        Text(
+            text = label + ":",
+            style = MaterialTheme.typography.labelSmall.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp
+            ),
+            color = customColor)
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 8.dp)
-                .clickable { expanded = true },
+                .clickable { expanded = true }
+                .background(Color(0xfff2f2f2), shape = MaterialTheme.shapes.small)
+                .padding(8.dp),
             contentAlignment = Alignment.CenterStart
         ) {
-            Text(text = selectedOption.toString(), color = MaterialTheme.colorScheme.onBackground)
+            Text(text = selectedOption.toString(), color = backgroundColor)
         }
         DropdownMenu(
             expanded = expanded,
@@ -234,7 +304,10 @@ fun <T> DropdownMenuBox(
                     onClick = {
                         onOptionSelected(option)
                         expanded = false
-                    }
+                    },
+                    modifier = Modifier
+                        .background(Color(0xfff2f2f2), shape = MaterialTheme.shapes.small)
+                        .padding(2.dp)
                 )
             }
         }
